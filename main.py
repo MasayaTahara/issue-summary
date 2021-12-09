348719f83a460a1ee81ada62700c46c1f8c34f20
MasayaTahara <mocoso.0u1@gmail.com>
2021-12-09 14:44:24 +0900

    Add sample for PyGitHub

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+from github import Github
+
+load_dotenv()
+TOKEN = os.environ['TOKEN']
+ORGANIZATION = os.environ['ORGANIZATION']
+REPOSITRY = os.environ['REPOSITRY']
+
+if __name__ == '__main__':
+    g = Github(TOKEN)
+
+    for _repo in g.get_organization(ORGANIZATION).get_repos(type='public'):
+        if _repo.name == REPOSITRY:
+            repo = _repo
+            break
+    print(repo)
+
+    for _issue in repo.get_issues():
+        print('  issue name: #{:<4} {}'.format(_issue.number, _issue.title))
