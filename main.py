6a90f7970ed71513aedfbc2b6b418731ce98e26e
MasayaTahara <mocoso.0u1@gmail.com>
2021-12-14 15:25:56 +0900

    Remove unsed dependency

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import os
-from typing import List
 from dotenv import load_dotenv
 from github import Github
-from github.Issue import Issue
 from github.PaginatedList import PaginatedList
 from github.Repository import Repository
 
